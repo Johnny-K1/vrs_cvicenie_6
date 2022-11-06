71aed8366c21d778cb46aece2396f448be394618
Johnny-K1 <jkosecky54@gmail.com>
2022-11-06 17:57:41 +0100

    add humidity and temperature

--- Src/main.c
+++ Src/main.c
@@ -25,15 +25,19 @@
 #include "usart.h"
 #include "lis3mdltr.h"
 #include "lsm6ds0.h"
+#include "hts221.h"
 #include "stdio.h"
 #include "string.h"
 #include "dma.h"
 
+
 #define CHAR_BUFF_SIZE	30
 
-uint8_t temp = 0;
+
 float mag[3], acc[3];
 char formated_text[30], value_x[10], value_y[10], value_z[10];
+float temp = 0.0;
+uint16_t humidity = 0;
 
 void SystemClock_Config(void);
 
@@ -53,13 +57,15 @@ int main(void)
   MX_USART2_UART_Init();
 
   lsm6ds0_init();
+  hts221_init();
 
   while (1)
   {
 	  //os			   x      y        z
-	  lsm6ds0_get_acc(acc, (acc+1), (acc+2));
+	  hts221_get_temp(&temp);
+	  hts221_get_humidity(&humidity);
 	  memset(formated_text, '\0', sizeof(formated_text));
-	  sprintf(formated_text, "%0.4f,%0.4f,%0.4f\r", acc[0], acc[1], acc[2]);
+	  sprintf(formated_text, "%.1f,%d,0\r", temp, humidity);
 	  USART2_PutBuffer((uint8_t*)formated_text, strlen(formated_text));
 	  LL_mDelay(10);
   }
